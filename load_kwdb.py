ea154db3c7786b6ddf441f52d84676cdf1ef3efc
Ted <beholder1@gmail.com>
2022-12-18 15:33:21 +0100

    Change kwdb to JSON for loading speed
    Average load time for yaml was 3.4s. For json it's 0.2s.

--- load_kwdb.py
+++ load_kwdb.py
@@ -1,17 +1,17 @@
 # coding: utf-8
 
 import sublime
-from ruamel.yaml import YAML
 
 KEYWORD_DATA = None
 
 def load_data():
-    sourcePath = 'Packages/{}/kwdb.yaml'.format(__package__)
+    path = 'Packages/{}/kwdb.json'.format(__package__)
+    data_file = sublime.load_resource(path)
     global KEYWORD_DATA
-    KEYWORD_DATA = YAML().load(sublime.load_resource(sourcePath))
-        
+    KEYWORD_DATA = sublime.decode_value(data_file)
+
 def plugin_loaded():
-    sublime.set_timeout_async(load_data, 2500)
+    sublime.set_timeout_async(load_data, 100)
 
 def plugin_unloaded():
     global KEYWORD_DATA
